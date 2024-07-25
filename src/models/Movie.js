6ba92d66d3e64d917c33b71e22e3410aa84d35a2
jadtfen <119529173+jadtfen@users.noreply.github.com>
2024-07-25 14:33:17 -0400

    Update Movie.js

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-  movieID: { type: Number, unique: true },
+  movieID: { type: Number, required: true, unique: true },
   title: { type: String, required: true },
   genre: { type: String, required: true },
   description: { type: String, required: true },
