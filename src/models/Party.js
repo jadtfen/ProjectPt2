1a0d25e82147a854ffc0ef54b83cacfe748693a1
jadtfen <119529173+jadtfen@users.noreply.github.com>
2024-07-25 14:33:29 -0400

    Update Party.js

--- src/models/Party.js
+++ src/models/Party.js
@@ -1,22 +1,11 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const PartySchema = new Schema({
-  partyName: {
-    type: String,
-    required: true,
-  },
-  hostID: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  partyInviteCode: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+const PartySchema = new mongoose.Schema({
+  partyName: { type: String, required: true },
+  hostID: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  partyInviteCode: { type: String, required: true, unique: true },
 });
 
 const Party = mongoose.model('Party', PartySchema, 'party');
+
 module.exports = Party;
