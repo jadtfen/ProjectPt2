37c779caa58ff101c7a29cb0521d7ff7fbf1f29a
jadtfen <jjaden562@gmail.com>
2024-07-26 19:45:30 -0400

    ghjk

--- src/server.js
+++ src/server.js
@@ -69,9 +69,9 @@ app.use('/api/poll', pollRouter);
 app.set('port', process.env.PORT || 5000);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+  app.use(express.static(path.join(__dirname, 'src', 'build')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'src', 'build', 'index.html'));
   });
 }
 
