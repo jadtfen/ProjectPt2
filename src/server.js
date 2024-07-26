34422b09399cbb27b37f281f57e3c69ccc37478e
jadtfen <jjaden562@gmail.com>
2024-07-26 17:12:25 -0400

    dddddd

--- src/server.js
+++ src/server.js
@@ -69,7 +69,7 @@ app.use('/api/poll', pollRouter);
 app.set('port', (process.env.PORT || 5000));
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
+    app.use(express.static('src/build'));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     });
