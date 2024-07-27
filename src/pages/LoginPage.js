f7ef8e26fab90b90cc1a0749ddfb9c9d8ff4f576
jadtfen <jjaden562@gmail.com>
2024-07-27 16:35:21 -0400

    new commit

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -13,9 +13,9 @@ function LoginPage() {
 
   function buildPath(route) {
     if (process.env.NODE_ENV === 'production') {
-      return 'https://' + app_name + '.herokuapp.com/' + route;
+      return `https://${app_name}.herokuapp.com/${route}`;
     } else {
-      return 'http://localhost:5000/' + route;
+      return `http://localhost:5000/${route}`;
     }
   }
 
